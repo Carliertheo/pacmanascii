5fb15021ec7bd150c915a15c7ac4d6647f9d8128
Theo Carlier <theo@MacBook-Pro-de-Theo.local>
2021-03-03 08:22:52 +0100

    Commit initial

---
+++ pacmanascii/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pacmanascii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
